f442385ba4f592792e69a2a51954c93599ed96c6
akankshawagh15 <68599265+akankshawagh15@users.noreply.github.com>
2024-03-01 13:04:12 +0530

    upload

---
+++ src/main/java/com/example/studentapi/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.example.studentapi.service;
+
+import java.util.List;
+
+import com.example.studentapi.entity.Student;
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+	
+	Student updateStudent(Student student);
+	
+	void deleteStudentById(Long id);
+}
